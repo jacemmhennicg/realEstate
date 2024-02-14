60a17e682c60df5edfdc809c1dc04cb588b97529
Jacem MHENNI <jacem.mhenni.cg@gmail.com>
2024-02-14 16:48:27 +0100

    test 7

--- RealEstate/Core/TabBar/MainTabView.swift
+++ RealEstate/Core/TabBar/MainTabView.swift
@@ -1,15 +1,8 @@
-//
-//  MainTabView.swift
-//  RealEstate
-//
-//  Created by Coding GOAT on 12/2/2024.
-//
-
 import SwiftUI
 
-struct MainTabView: View {@State private var activeTab: Tab = .explore
-    
+struct MainTabView: View {
     @Namespace private var animation
+    @State private var activeTab: Tab = .explore
     @State private var tabShapePosition: CGPoint = .zero
     
     init() {
@@ -20,17 +13,12 @@ struct MainTabView: View {@State private var activeTab: Tab = .explore
             TabView(selection: $activeTab){
                 ExploreView()
                     .tag(Tab.explore)
-                //.toolbar(.hidden, for: .tabBar)
-                
+              
                 WishlistsView()
                     .tag(Tab.wishlists)
-                // .toolbar(.hidden, for: .tabBar)
-                
-               ProfileView()
+                ProfileView()
                     .tag(Tab.profile)
-                //  .toolbar(.hidden, for: .tabBar)
-                
-                
+   
             }
             
             CustomTabBar()
