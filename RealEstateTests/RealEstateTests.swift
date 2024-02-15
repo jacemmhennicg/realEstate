d24ae2c3849631881f3c5af0ad627a765e0c9174
Jacem MHENNI <jacem.mhenni.cg@gmail.com>
2024-02-15 10:12:09 +0100

    fix tests

--- RealEstateTests/RealEstateTests.swift
+++ RealEstateTests/RealEstateTests.swift
@@ -10,13 +10,9 @@ import XCTest
 
 final class RealEstateTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+
 
     func testExample() throws {
         // This is an example of a functional test case.
@@ -28,11 +24,6 @@ final class RealEstateTests: XCTestCase {
         XCTAssert(a == 11)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
