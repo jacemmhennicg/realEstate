938da214b4b45d722eccff2e052b66d2becc1486
Jacem MHENNI <jacem.mhenni.cg@gmail.com>
2024-02-16 10:29:48 +0100

    add test class

---
+++ RealEstateTests/TestClass.swift
@@ -0,0 +1,50 @@
+//
+//  TestClass.swift
+//  RealEstateTests
+//
+//  Created by Coding GOAT on 16/2/2024.
+//
+
+import XCTest
+@testable import RealEstate
+
+class TestClass: XCTestCase {
+    
+    override class func setUp() {
+        super.setUp()
+        print("setUpAll")
+    }
+    
+    override func setUp() {
+        super.setUp()
+        print("setUp")
+    }
+    
+    override func tearDown() {
+        print("tearDown")
+        super.tearDown()
+    }
+    
+    override class func tearDown() {
+        print("tearDownAll")
+        super.tearDown()
+    }
+    
+    func testSum() {
+        XCTAssertEqual(sum(a: 5, b: 6), 5 + 6)
+    }
+    
+    func testSubstract() {
+        XCTAssertEqual(substract(a: 10, b: 3), 10 - 3)
+    }
+    
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    func substract(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
+
+
